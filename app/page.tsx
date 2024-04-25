093700e74e3fe1a65eea3e0846efc1870bc2e5ee
Aadarsh singh <bond007spectre316@gmail.com>
2024-04-25 15:19:13 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 // page.tsx
 "use client"
 // page.tsx
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import dynamic from 'next/dynamic';
 import BackToTopButton from '../components/ToTop';
 import Footer from '../components/Footer';
 import Pagep1 from './comp1'
 import BackgroundWithStars from './BackgroundWithStars';
 import Lotcomp2 from './lotcomp2';
-
 
 const DynamicParticles = dynamic(() => import('@tsparticles/react'), { ssr: false });
 
@@ -20,19 +19,19 @@ import { loadCurvesPath } from '@tsparticles/path-curves';
 const Page = () => {
   console.log('Rendering Page component');
 
- 
-  const footerStyle = {
+  // Define the type for the style object
+  const footerStyle: CSSProperties = {
     position: 'absolute',
     top: '3120px',
     width: '100%',
     zIndex: 2,
   };
+
   const videos = [
     { source: "/Videos/Black_Hole_Decay.mp4", title: "Black Hole Hawking Evaporation" },
     { source: "/Videos/Black_Hole.mp4", title: "Intergalactic Black Hole" },
     { source: "/Videos/Gravitational_Waves.mp4", title: "Gravitational Waves from two Black Holes" },
   ];
-
 
   const videos2 = [
     { source: "/Videos/SM.mp4", title: "Black Hole Hawking Evaporation" },
@@ -55,44 +54,41 @@ const Page = () => {
     });
   }, []);
 
-return (
-  <>
- <Pagep1 />
-
- <Lotcomp2 />
-    <div>
-    <BackgroundWithStars videos={videos} />
-    <BackgroundWithStars videos={videos2} />
-    </div>
-  
+  return (
+    <>
+      <Pagep1 />
+      <Lotcomp2 />
+      <div>
+        <BackgroundWithStars videos={videos} />
+        <BackgroundWithStars videos={videos2} />
+      </div>
       {/* Add the BackToTopButton component */}
       <BackToTopButton />
-    <Footer style={footerStyle} />
-    <style>
-          {`
-            /* For WebKit (Chrome, Safari, etc.) */
-            ::-webkit-scrollbar {
-              width: 10px;
-              border-radius: 5px; /* Roundness of the scrollbar track */
-            }
-            
-            ::-webkit-scrollbar-track {
-              background: rgba(255, 255, 255, 0); /* Background color of the track */
-            }
-            
-            ::-webkit-scrollbar-thumb {
-              background: #B0C4DE; /* Color of the thumb */
-              border-radius: 5px; /* Roundness of the thumb */
-            }
+      <Footer style={footerStyle} />
+      <style>
+        {`
+          /* For WebKit (Chrome, Safari, etc.) */
+          ::-webkit-scrollbar {
+            width: 10px;
+            border-radius: 5px; /* Roundness of the scrollbar track */
+          }
+          
+          ::-webkit-scrollbar-track {
+            background: rgba(255, 255, 255, 0); /* Background color of the track */
+          }
+          
+          ::-webkit-scrollbar-thumb {
+            background: #B0C4DE; /* Color of the thumb */
+            border-radius: 5px; /* Roundness of the thumb */
+          }
 
-            /* For Firefox */
-            scrollbar-width: thin;
-            scrollbar-color: #B0C4DE rgba(255, 255, 255, 1); /* Color of the thumb and track */
-          `}
-        </style>
-  </>
-);
-
+          /* For Firefox */
+          scrollbar-width: thin;
+          scrollbar-color: #B0C4DE rgba(255, 255, 255, 1); /* Color of the thumb and track */
+        `}
+      </style>
+    </>
+  );
 };
 
 export default Page;
